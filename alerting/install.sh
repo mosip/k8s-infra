741869662f2ca92f021cb5f4085b6e6b91895101
Binyam <binseyoum@gmail.com>
2022-06-27 13:05:55 +0300

    add alerting

---
+++ alerting/install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Patch notification alerts 
+
+echo Patching alert manager secrets 
+kubectl patch secret alertmanager-rancher-monitoring-alertmanager -n cattle-monitoring-system  --patch="{\"data\": { \"alertmanager.yaml\": \"$(cat ./alertmanager.yaml |base64)\" }}"
+echo Regenerating secrets
+kubectl delete secret alertmanager-rancher-monitoring-alertmanager-generated -n cattle-monitoring-system
+echo Adding cluster name
+kubectl patch Prometheus rancher-monitoring-prometheus -n cattle-monitoring-system --patch-file patch-cluster-name.yaml --type=merge
+echo Applying custom alerts
+kubectl apply -f custom-alerts/ 
