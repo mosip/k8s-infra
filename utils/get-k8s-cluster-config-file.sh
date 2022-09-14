a89c18632a446d094ab889a7fcce15745b9d66d7
syed-salman-technoforte <syed.salman@technoforte.co.in>
2022-09-14 23:01:01 +0530

    [ MOSIP-23928 ] update get-k8s-cluster-config-file.sh

--- utils/get-k8s-cluster-config-file.sh
+++ utils/get-k8s-cluster-config-file.sh
@@ -10,30 +10,24 @@ fi
 FILENAME=~/$ENV_NAME-kube-config-cluster.yml;
 INTERFACE_NAME="eth0";
 IP_ADDRESS=$( ip addr show $INTERFACE_NAME | grep -w $INTERFACE_NAME | awk 'NR==2{print $2}' | awk -F '/' '{print $1}' );
-CERTIFICATE_AUTHORITY=$( cat "/etc/kubernetes/ssl/kube-ca.pem" | base64  | sed -E ':a;N;$!ba;s/\r{0,1}\n//g' )
-CLIENT_CERTIFICATE=$( cat "/etc/kubernetes/ssl/kube-controller-manager.pem" | base64  | sed -E ':a;N;$!ba;s/\r{0,1}\n//g' )
-CLIENT_KEY=$( cat "/etc/kubernetes/ssl/kube-controller-manager-key.pem" | base64  | sed -E ':a;N;$!ba;s/\r{0,1}\n//g' )
+CERTIFICATE_AUTHORITY=$( base64 "/etc/kubernetes/ssl/kube-ca.pem" | sed -E ':a;N;$!ba;s/\r{0,1}\n//g' )
+CLIENT_CERTIFICATE=$( base64  "/etc/kubernetes/ssl/kube-controller-manager.pem" | sed -E ':a;N;$!ba;s/\r{0,1}\n//g' )
+CLIENT_KEY=$( base64 "/etc/kubernetes/ssl/kube-controller-manager-key.pem" | sed -E ':a;N;$!ba;s/\r{0,1}\n//g' )
 
-cat <<< "apiVersion: v1
-kind: Config
-clusters:
-- cluster:
-    api-version: v1
-    certificate-authority-data: $CERTIFICATE_AUTHORITY
-    server: \"https://$IP_ADDRESS:6443\"
-  name: \"$ENV_NAME\"
-contexts:
-- context:
-    cluster: \"$ENV_NAME\"
-    user: \"kube-admin-local\"
-  name: \"$ENV_NAME\"
-current-context: \"$ENV_NAME\"
-users:
-- name: \"kube-admin-local\"
-  user:
-    client-certificate-data: $CLIENT_CERTIFICATE
-    client-key-data: $CLIENT_KEY" > "$FILENAME";
+# copy kube node yaml file
+sudo cp "/etc/kubernetes/ssl/kubecfg-kube-node.yaml" "$FILENAME";
+
+sed -i 's/certificate-authority:.*/certificate-authority-data:/g' "$FILENAME";
+sed -i 's/client-certificate:.*/client-certificate-data:/g' "$FILENAME";
+sed -i 's/client-key:.*/client-key-data:/g' "$FILENAME";
+
+sed -i "s/127.0.0.1/$IP_ADDRESS/g" "$FILENAME";
+sed -i "s/\"local\"/\"$ENV_NAME\"/g" "$FILENAME";
+
+sed -i "s/certificate-authority-data:/certificate-authority-data: $CERTIFICATE_AUTHORITY/g" "$FILENAME";
+sed -i "s/client-certificate-data:/client-certificate-data: $CLIENT_CERTIFICATE/g" "$FILENAME";
+sed -i "s/client-key-data:/client-key-data: $CLIENT_KEY/g" "$FILENAME";
 
 echo "INTERFACE NAME : $INTERFACE_NAME";
 echo "IP ADDRESS : $IP_ADDRESS";
-echo "KUBENETES CLUSTER CONFIG FILE : $FILENAME ";
+echo "CREATED KUBERNETES CLUSTER CONFIG FILE : $FILENAME ";
