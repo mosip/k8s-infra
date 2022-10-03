816a2f67738ee9fe7848c3e7d69a8c485d4c60a6
gaganama <anjinkumars@gmail.com>
2022-10-03 10:54:12 +0530

    [MOSIP-9248] scripts to save certificates.

---
+++ utils/download-certs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash    
+# Read the user input   
+
+read -p "Enter the ip of remote node: " ip  
+read -p "Enter the user of remote node: " remote_user  
+read -p "Enter the pem file path: "  pem
+echo
+ssh -i ${pem} ${remote_user}@${ip} "bash -c 'cd /etc && zip -r letsencrypt.zip letsencrypt'"
+scp -i ${pem} -r ${remote_user}@${ip}:/etc/letsencrypt.zip .
+ssh -i ${pem} ${remote_user}@${ip} rm /etc/letsencrypt.zip
