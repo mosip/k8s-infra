5e69024185c19f5cdfb2012b48881ac37f183d5c
bhumi46 <111699703+bhumi46@users.noreply.github.com>
2023-04-05 12:16:30 +0530

    DSD-2357 (#56)
    * longhorn_chart_version_updated
    
    * longhorn stable version updated
    
    Signed-off-by: ckm007 <chandrakeshavmishra@gmail.com>

--- longhorn/pre_install.sh
+++ longhorn/pre_install.sh
@@ -6,9 +6,9 @@ echo Create $NS namespace
 kubectl create namespace $NS
 
 echo Installing iscsi
-kubectl apply -n $NS -f https://raw.githubusercontent.com/longhorn/longhorn/v1.2.3/deploy/prerequisite/longhorn-iscsi-installation.yaml
+kubectl apply -n $NS -f https://raw.githubusercontent.com/longhorn/longhorn/v1.4.1/deploy/prerequisite/longhorn-iscsi-installation.yaml
 
 echo Installing nfsv4 client
-kubectl apply -n $NS -f https://raw.githubusercontent.com/longhorn/longhorn/v1.2.3/deploy/prerequisite/longhorn-nfs-installation.yaml
+kubectl apply -n $NS -f https://raw.githubusercontent.com/longhorn/longhorn/v1.4.1/deploy/prerequisite/longhorn-nfs-installation.yaml
 
 echo Pre-requisites for longhorn are installed
