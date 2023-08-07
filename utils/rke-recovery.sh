3445d47f53584320d25b7f8bc3b7f345d9837405
syed-salman-technoforte <syed.salman@technoforte.co.in>
2023-08-07 13:59:54 +0530

    [DSD-3206] Updated rke-recovery.sh file

--- utils/rke-recovery.sh
+++ utils/rke-recovery.sh
@@ -12,33 +12,7 @@ sudo chmod +x kubectl
 sudo mv kubectl /bin/kubectl
 
 echo "Building cluster.yml..."
-echo "Working on Nodes..."
-echo 'nodes:' > cluster.yml
-kubectl -n kube-system get configmap full-cluster-state -o json | jq -r .data.\"full-cluster-state\" | jq -r .desiredState.rkeConfig.nodes | yq -P | sed 's/^/  /' | \
-sed -e 's/internalAddress/internal_address/g' | \
-sed -e 's/hostnameOverride/hostname_override/g' | \
-sed -e 's/sshKeyPath/ssh_key_path/g' >> cluster.yml
-echo "" >> cluster.yml
-
-echo "Working on services..."
-echo 'services:' >> cluster.yml
-kubectl -n kube-system get configmap full-cluster-state -o json | jq -r .data.\"full-cluster-state\" | jq -r .desiredState.rkeConfig.services | yq -P | sed 's/^/  /' >> cluster.yml
-echo "" >> cluster.yml
-
-echo "Working on network..."
-echo 'network:' >> cluster.yml
-kubectl -n kube-system get configmap full-cluster-state -o json | jq -r .data.\"full-cluster-state\" | jq -r .desiredState.rkeConfig.network | yq -P | sed 's/^/  /' >> cluster.yml
-echo "" >> cluster.yml
-
-echo "Working on authentication..."
-echo 'authentication:' >> cluster.yml
-kubectl -n kube-system get configmap full-cluster-state -o json | jq -r .data.\"full-cluster-state\" | jq -r .desiredState.rkeConfig.authentication | yq -P | sed 's/^/  /' >> cluster.yml
-echo "" >> cluster.yml
-
-echo "Working on systemImages..."
-echo 'system_images:' >> cluster.yml
-kubectl -n kube-system get configmap full-cluster-state -o json | jq -r .data.\"full-cluster-state\" | jq -r .desiredState.rkeConfig.systemImages | yq -P | sed 's/^/  /' >> cluster.yml
-echo "" >> cluster.yml
+kubectl -n kube-system get configmap full-cluster-state -o json| jq -r .data.\"full-cluster-state\" | jq -r .desiredState.rkeConfig | yq -P > cluster.yml
 
 echo "Building cluster.rkestate..."
 kubectl -n kube-system get configmap full-cluster-state -o json | jq -r .data.\"full-cluster-state\" | jq -r . > cluster.rkestate
