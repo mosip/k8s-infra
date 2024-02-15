72266a96b96b9b3cb897ecdeae2d42785cedc001
syed salman <syedsalman3753@gmail.com>
2024-02-15 15:43:29 +0530

    [DSD-4606] Added deployment-busybox-curl.yaml to httpbin install script
    Signed-off-by: syed salman <syedsalman3753@gmail.com>
    (cherry picked from commit 085190c827cb9cd5540c65bdc8f7190575a65a40)
    
    Signed-off-by: bhumi46 <thisisbn46@gmail.com>

--- utils/httpbin/install.sh
+++ utils/httpbin/install.sh
@@ -10,5 +10,6 @@ kubectl create ns $NS
 kubectl label ns $NS istio-injection=enabled --overwrite
 
 kubectl -n $NS apply -f svc.yaml
-kubectl -n $NS apply -f deployment.yaml 
+kubectl -n $NS apply -f deployment.yaml
+kubectl -n $NS apply -f deployment-busybox-curl.yaml
 kubectl -n $NS apply -f vs.yaml
