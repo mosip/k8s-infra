1e5b35dc9fe3b5a8dd857957843d6feb9000a252
ckm007 <chandrakeshavmishra@gmail.com>
2024-08-09 14:02:59 +0530

    [MOSIP-34911] moved longhorn  configuration  steps to storage-class
    Signed-off-by: ckm007 <chandrakeshavmishra@gmail.com>

---
+++ storage-class/longhorn/delete.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# Uninstalls longhorn
+## Usage: ./delete.sh [kubeconfig]
+
+if [ $# -ge 1 ] ; then
+  export KUBECONFIG=$1
+fi
+
+NS=longhorn-system
+
+while true; do
+    echo "$(tput setaf 3 )[ WARNING ] Services using longhorn as a storage class will lose their data.";
+    echo "Back up the data and uninstall the required services, then you can proceed with the uninstallation."$(tput sgr0);
+    read -p "Are you sure you want to delete longhorn helm charts?(Y/n) " yn
+    if [ $yn = "Y" ]
+      then
+        echo Removing longhorn
+        helm -n $NS delete longhorn
+        echo Removing iscsi
+        kubectl delete -n $NS -f https://raw.githubusercontent.com/longhorn/longhorn/v1.4.2/deploy/prerequisite/longhorn-iscsi-installation.yaml
+        echo Removing nfsv4 client
+        kubectl delete -n $NS -f https://raw.githubusercontent.com/longhorn/longhorn/v1.4.2/deploy/prerequisite/longhorn-nfs-installation.yaml
+        break
+      else
+        break
+    fi
+done
---
+++ storage-class/longhorn/install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+## Installs longhorn
+## Usage: ./install.sh [kubeconfig]
+
+if [ $# -ge 1 ] ; then
+  export KUBECONFIG=$1
+fi
+
+NS=longhorn-system
+CHART_VERSION=1.4.2
+
+echo "Adding Longhorn helm repo"
+helm repo add longhorn https://charts.longhorn.io
+helm repo update
+
+echo "Installing Longhorn"
+helm install longhorn longhorn/longhorn -n $NS               \
+--create-namespace --version $CHART_VERSION                  \
+--set defaultSettings.guaranteedEngineManagerCPU=5           \
+--set defaultSettings.guaranteedReplicaManagerCPU=5          \
+--set persistence.defaultClassReplicaCount=1                 \
+--set defaultSettings.defaultReplicaCount=1
+
+echo "Longhorn Installed !!!"
