0afce78aa5e0155c97ec94d70f9f0c1faf3ec9fc
techno-376 <rakshitham38@gmail.com>
2025-01-06 19:30:50 +0530

    [MOSIP-38708]
    Signed-off-by: techno-376 <rakshitham38@gmail.com>

--- logging/install.sh
+++ logging/install.sh
@@ -32,39 +32,35 @@ function check_and_update_kibana_host() {
 check_and_update_kibana_host
 
 function installing_logging() {
-  echo "Updating Helm repositories."
+  echo Updating helm repos
   helm repo add bitnami https://charts.bitnami.com/bitnami
   helm repo add banzaicloud-stable https://charts.helm.sh/stable
   helm repo update
 
-  echo "Installing Bitnami Elasticsearch and Kibana istio objects."
+  echo Installing Bitnami Elasticsearch and Kibana istio objects
   helm -n $NS install elasticsearch mosip/elasticsearch -f es_values.yaml --version 17.9.25 --wait
-  echo "Installed Bitnami Elasticsearch and Kibana istio objects."
+  echo Installed Bitnami Elasticsearch and Kibana istio objects
 
+  KIBANA_HOST=$(kubectl get cm global -o jsonpath={.data.mosip-kibana-host})
   KIBANA_NAME=elasticsearch-kibana
 
-  echo "Installing Istio Addons."
-  helm -n $NS install istio-addons chart/istio-addons \
-    --set kibanaHost=$KIBANA_HOST \
-    --set installName=$KIBANA_NAME
+  echo Install istio addons
+  helm -n $NS install istio-addons chart/istio-addons --set kibanaHost=$KIBANA_HOST --set installName=$KIBANA_NAME
 
-  echo "Installing CRDs for Logging Operator."
+  echo Installing crds for logging operator
   helm -n $NS install rancher-logging-crd mosip/rancher-logging-crd --wait
-  echo "Installed CRDs for Logging Operator."
-
-  echo "Installing Logging Operator."
+  echo Installed crds for logging operator
+  echo Installing logging operator
   helm -n $NS install rancher-logging mosip/rancher-logging -f values.yaml
-  echo "Installed Logging Operator."
-
+  echo Installed logging operator
   return 0
 }
 
-# Set commands for error handling
+# set commands for error handling.
 set -e
-set -o errexit   ## Exit the script if any statement returns a non-true return value
-set -o nounset   ## Exit the script if you try to use an uninitialized variable
-set -o errtrace  # Trace ERR through 'time command' and other functions
-set -o pipefail  # Trace ERR through pipes
-
-installing_logging   # Calling the function
+set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
+set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
+set -o errtrace  # trace ERR through 'time command' and other functions
+set -o pipefail  # trace ERR through pipes
+installing_logging   # calling function
 
