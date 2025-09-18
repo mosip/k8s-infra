015c72a2ebb37687bb2bfb79bec311d6aaec369e
bhumi46 <thisisbn46@gmail.com>
2025-09-19 02:09:36 +0530

    [MOSIP-43098]resolved known bugs in docs and updated  rancher ui and keyclaok script for obs cluster
    Signed-off-by: bhumi46 <thisisbn46@gmail.com>

--- observation/keycloak/install.sh
+++ observation/keycloak/install.sh
@@ -16,9 +16,16 @@ NS=keycloak
 echo Creating namespace
 kubectl create ns keycloak
 
+helm repo add mosip https://mosip.github.io/mosip-helm
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
 echo Installing
-helm -n $NS install keycloak mosip/keycloak --version "7.1.18" -f values.yaml --set ingress.hostname=$1
+helm -n $NS template keycloak mosip/keycloak --version "7.1.18" -f values.yaml \
+--set image.repository=mosipid/mosip-artemis-keycloak \
+--set image.tag=1.2.0.1 \
+--set image.pullPolicy=Always \
+--set postgresql.image.repository="mosipint/postgresql" \
+--set postgresql.image.tag="14.2.0-debian-10-r70" \
+--set ingress.hostname=$1
 
